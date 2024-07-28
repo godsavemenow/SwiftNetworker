a20156f36e51748d5aa6b9fd3b721ba45be596bd
Lucas Silva <lvjs.contact@gmail.com>
2024-07-28 07:38:37 -0300

    feat: create client

--- swift-networker/Sources/swift-networker/SwiftNetworker.swift
+++ swift-networker/Sources/swift-networker/SwiftNetworker.swift
@@ -1,8 +1,105 @@
 //
-//  File.swift
+//  SwiftNetworker.swift
 //  
 //
-//  Created by Lucas Silva on 28/07/24.
+//  Created by Lucas Silva
 //
 
 import Foundation
+
+public class Networker {
+    
+    let errorHandler: ErrorHandler
+    let logger: Logger
+    
+    init(errorHandler: ErrorHandler = ErrorHandler(),
+         logger: Logger = Logger()
+    ) {
+        self.errorHandler = errorHandler
+        self.logger = logger
+    }
+    
+    public func perform<T: Decodable>(_ request: NetworkRequest, responseModel: T?, completion: @escaping (Result<Response<T>, NetworkError>) -> Void) {
+        let urlRequest = setURLRequest(request)
+        
+        logger.logRequest(urlRequest)
+        
+        let task = URLSession.shared.dataTask(with: urlRequest) { [weak self]
+            data, response, error in
+            guard let self else { return }
+            
+            if let error = error {
+                logger.logError(error)
+                completion(.failure(errorHandler.handle(error, response: response)))
+                return
+            }
+            
+            guard let data = data, let response = response else {
+                let customError: NetworkError = .noData
+                logger.logError(customError)
+                completion(.failure(customError))
+                return
+            }
+            
+            if let httpResponse = response as? HTTPURLResponse, !HTTPStatusCode.isSuccessful(httpResponse.statusCode) {
+                let error = errorHandler.handle(nil, response: response)
+                logger.logError(error)
+                completion(.failure(error))
+                return
+            }
+            
+            let networkResponse = NetworkResponse(data: data, response: response)
+            let responses = handleResponse(networkResponse: networkResponse, responseModel: responseModel.self)
+            completion(responses)
+        }
+        
+        task.resume()
+    }
+
+    
+    @available(iOS 15.0, macOS 12.0, *)
+    public func perform<T: Decodable>(_ request: NetworkRequest, responseModel: T?) async -> Result<Response<T>, NetworkError> {
+        let urlRequest = setURLRequest(request)
+        
+        do {
+            let (data, response) = try await URLSession.shared.data(for: urlRequest)
+            
+            guard let httpResponse = response as? HTTPURLResponse, HTTPStatusCode.isSuccessful(httpResponse.statusCode) else {
+                let customError = errorHandler.handle(nil, response: response)
+                logger.logError(customError)
+                return .failure(customError)
+            }
+            
+            let networkResponse = NetworkResponse(data: data, response: response)
+            let responses = handleResponse(networkResponse: networkResponse, responseModel: responseModel.self)
+            return responses
+        } catch let error {
+            let customError = errorHandler.handle(error, response: nil)
+            logger.logError(customError)
+            return .failure(customError)
+        }
+    }
+    
+    private func handleResponse<T: Decodable>(networkResponse: NetworkResponse, responseModel: T?) -> Result<Response<T>, NetworkError> {
+        
+        let decoder = JSONDecoder()
+        do {
+            let decodedObject = try decoder.decode(T.self, from: networkResponse.data)
+            let response = Response(networkResponse: networkResponse, decodedResponse: decodedObject)
+            logger.logResponse(response)
+            return .success(response)
+        } catch let error {
+            let customError = errorHandler.handle(error, response: nil)
+            return .failure(customError)
+        }
+    }
+        
+    private func setURLRequest(_ request: NetworkRequest) -> URLRequest {
+        var urlRequest = URLRequest(url: request.url)
+        urlRequest.httpMethod = request.method.rawValue
+        urlRequest.allHTTPHeaderFields = request.headers
+        urlRequest.httpBody = request.body
+        
+        return urlRequest
+    }
+}
--- swift-networker/Tests/Swift-NetworkerTests/swift_networkerTests.swift
+++ swift-networker/Tests/Swift-NetworkerTests/swift_networkerTests.swift
@@ -1,11 +1,11 @@
 import XCTest
-@testable import swift_networker
+@testable import Swift_Networker
 
-final class swift_networkerTests: XCTestCase {
+final class Swift_NetworkerTests: XCTestCase {
     func testExample() throws {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(swift_networker().text, "Hello, World!")
+        XCTAssertEqual("Hello, World!", "Hello, World!")
     }
 }
